8169e01f09f3efbb6d92ce9b0dc1a384867ab55b
Mloirat <m.loirat@hotmail.fr>
2023-01-20 14:03:30 +0100

    initialisation

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import cross from "./crossDelete.png";
 
 function App() {
-  const [tasks, setTasks] = React.useState([]);
-  const [count, setCount] = React.useState(0);
 
-      function addItem() {
-        const name = document.getElementById("myInput").value;
-
-        const newTask = {
-          id: Math.floor(Math.random()*1000),
-          value: name
-      }
-      /*
-        const itemIds = tasks.map((i) => i.id);
-        setTasks([...tasks, allTasks.find((i) => !itemIds.includes(i.id))]);
-            */
-      setTasks([...tasks, newTask]);
-      setCount(tasks.length);
-      }
-
-      function removeItem(task) {
-        setTasks(tasks.filter((i) => i.id !== task.id));
-        setCount(tasks.length);      
-      }
-      
-      function CountDisplay(props){
-        return <div className='titleToDos'>You have {props.children} Todos</div>
-      }
-
-      return (
-        <div className="keys">
-          <CountDisplay children={count}/>
-          <hr></hr>
-          <ul style={{ listStyle: "none", paddingLeft: 0 }}>
-            {tasks.map((task) => (
-              // Ajouter la propriété key à l'élément <li>, lui donner la valeur de item.id
-              <li>
-                <label>{task.value}</label>
-                <img src={cross} className='crossDelete' alt="React Image" onClick={() => removeItem(task)} />
-                <hr></hr>
-              </li>
-
-            ))}
-          </ul>
-          <input placeholder='Enter Item' id="myInput"></input>
-          <button onClick={addItem}>Submit</button>
-
-        </div>
-      );
-
-
-
-
-
-
-
-
-
+  function clickTest() {
+    console.log("Bouton");
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <boutton>Add Task</boutton>
+        <div className='AppContent'>  
+          <label for="searchInput">Search the site:</label>
+          <input type="search" id="searchInput" placeholder='Rechercher'></input>
+          <button onClick={clickTest}>Submit</button>
+        </div>
       </header>
     </div>
   );
