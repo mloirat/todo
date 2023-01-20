2068d54bb95181f1cce4ceb95357b5e691c1fd40
Mloirat <m.loirat@hotmail.fr>
2023-01-20 11:25:59 +0100

    startingchanges with '#' will be ignored, and an empty message aborts the commit.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import cross from "./crossDelete.png";
 
 function App() {
+  const [tasks, setTasks] = React.useState([]);
+  const [count, setCount] = React.useState(0);
+
+      function addItem() {
+        const name = document.getElementById("myInput").value;
+
+        const newTask = {
+          id: Math.floor(Math.random()*1000),
+          value: name
+      }
+      /*
+        const itemIds = tasks.map((i) => i.id);
+        setTasks([...tasks, allTasks.find((i) => !itemIds.includes(i.id))]);
+            */
+      setTasks([...tasks, newTask]);
+      setCount(tasks.length);
+      }
+
+      function removeItem(task) {
+        setTasks(tasks.filter((i) => i.id !== task.id));
+        setCount(tasks.length);      
+      }
+      
+      function CountDisplay(props){
+        return <div className='titleToDos'>You have {props.children} Todos</div>
+      }
+
+      return (
+        <div className="keys">
+          <CountDisplay children={count}/>
+          <hr></hr>
+          <ul style={{ listStyle: "none", paddingLeft: 0 }}>
+            {tasks.map((task) => (
+              // Ajouter la propriété key à l'élément <li>, lui donner la valeur de item.id
+              <li>
+                <label>{task.value}</label>
+                <img src={cross} className='crossDelete' alt="React Image" onClick={() => removeItem(task)} />
+                <hr></hr>
+              </li>
+
+            ))}
+          </ul>
+          <input placeholder='Enter Item' id="myInput"></input>
+          <button onClick={addItem}>Submit</button>
+
+        </div>
+      );
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <boutton>Add Task</boutton>
       </header>
     </div>
   );
